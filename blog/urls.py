78f5a098253c047dc6a552493774d258ca0c69b9
Scott Willrich <swillrich@gmail.com>
2022-09-21 09:02:19 -0500

    added post-detail page including css

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail,
-         name="posts-detail-page"),  # /posts/my-first-post
+         name="post-detail-page"),  # /posts/my-first-post
 ]
